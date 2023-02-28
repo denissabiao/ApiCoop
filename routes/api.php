f5272ffd419e307f1453b02d7960eae2f91a5e66
denissabiao <denis.sabiao@gmail.com>
2023-02-28 16:22:12 -0300

    Alter routes auth

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
-use App\Http\Controllers\ConsultaController;
-use App\Http\Controllers\MedicoController;
+use App\Http\Controllers\{
+    ConsultaController,
+    LoginController,
+    MedicoController
+};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,23 +19,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('consultas')->group(function () {
+Route::prefix('consulta')->group(function () {
     Route::get('/especialidade', [ConsultaController::class, 'buscaConsultaPelaEspecialidadeEData']);
     Route::get('/medico', [ConsultaController::class, 'getAppointmentByMedic']);
 
 });
 Route::apiResource('consulta', ConsultaController::class)->only('show');
 
-Route::prefix('medicos')->group(function () {
+Route::prefix('medico')->group(function () {
     Route::get('/especialidade/{especialidade}', [MedicoController::class, 'buscaMedicoPelaEspecialidade']);
 });
+Route::apiResource('medico', MedicoController::class)->only('show', 'index');
 
-Route::apiResource('medico', MedicoController::class)->only('show','index');
-
-Route::get('/users', function (Request $request) {
-    return 'funciona!';
-});
+Route::post('/login', [LoginController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/users', function (Request $request) {
     return 'funciona!';
 });
