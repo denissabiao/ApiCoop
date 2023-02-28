58f9a36dcf11295ed7ea354ad27b7baf34e3cb05
denissabiao <denis.sabiao@gmail.com>
2023-02-28 13:10:44 -0300

    Add Completed ConsultaService

--- app/Services/ConsultaService.php
+++ app/Services/ConsultaService.php
@@ -8,6 +8,8 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
+use function PHPSTORM_META\map;
+
 class ConsultaService extends Model
 {
     public static function getAppointmentDateLimit(string $data, int $limite)
@@ -24,6 +26,27 @@ class ConsultaService extends Model
             ->orderBy('data')
             ->orderBy('horario')
             ->get());
+    }
+
+    public function mapAppointmentMedic($consulta_medico)
+    {
+        $novo_array = [];
+        $consulta_medico->map(function ($item) use (&$novo_array) {
+            self::addItemToNewArray($novo_array, $item);
+        });
+
+        return $novo_array;
+    }
+
+    private static function addItemToNewArray(&$novo_array, $item)
+    {
+        $data = $item['data'];
+        $id = $item['id'];
+        $horario = $item['horario'];
+
+        $novo_array[$data][] = [$id => $horario];
+        $novo_array[$data]['quantidade_horario'] = array_key_last($novo_array[$data]) + 1;
+        $novo_array[$data]['medico'] = $item['medico'];
     }
 
 }
