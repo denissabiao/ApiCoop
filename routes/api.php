2ba86e09b5bdc83264f265959077852623be3241
denissabiao <denis.sabiao@gmail.com>
2023-02-28 17:02:47 -0300

    Add route especialidade

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\{
     ConsultaController,
+    EspecialidadeController,
     LoginController,
     MedicoController
 };
@@ -22,21 +23,18 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('consulta')->group(function () {
     Route::get('/especialidade', [ConsultaController::class, 'buscaConsultaPelaEspecialidadeEData']);
     Route::get('/medico', [ConsultaController::class, 'getAppointmentByMedic']);
-
-});
-Route::apiResource('consulta', ConsultaController::class)->only('show');
+})->apiResource('consulta', ConsultaController::class)->only('show');
 
 Route::prefix('medico')->group(function () {
     Route::get('/especialidade/{especialidade}', [MedicoController::class, 'buscaMedicoPelaEspecialidade']);
-});
-Route::apiResource('medico', MedicoController::class)->only('show', 'index');
+})->apiResource('medico', MedicoController::class)->only('show', 'index'); //busca automaticamente os metodos padroes
+
+Route::prefix('especialidade')->group(function () {
+
+})->apiResource('especialidade', EspecialidadeController::class)->only('index');
 
 Route::post('/login', [LoginController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::get('/users', function (Request $request) {
-    return 'funciona!';
 });
