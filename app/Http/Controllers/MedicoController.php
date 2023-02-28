0fbbd98921ef08bd4915afbc96c71c3750898dbf
denissabiao <denis.sabiao@gmail.com>
2023-02-28 11:06:13 -0300

    inject medic service denpedence

--- app/Http/Controllers/MedicoController.php
+++ app/Http/Controllers/MedicoController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\Medico as ResourcesMedico;
 use App\Models\Medico;
+use App\Services\MedicoService;
 use Illuminate\Http\Request;
 
 class MedicoController extends Controller
 {
+    protected $medicoService;
+
+
+    public function __construct(MedicoService $medicoService)
+    {
+        $this->medicoService = $medicoService;
+    }
 
     public function index()
     {
