16fca75f105a2986da02cfc1c4aac0cb050c8777
denissabiao <denis.sabiao@gmail.com>
2023-03-01 16:17:11 -0300

    Create User Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,24 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\User as ResourcesUser;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
     }
 
     /**
@@ -27,15 +22,70 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $messages = [
+            'Cpf.required' => 'Cpf é um campo obrigatório.',
+            'email.required' => 'Email é um campo obrigatório.',
+            'password.required' => 'Senha é um campo obrigatório.',
+            'data_nascimento.required' => 'Data Nascimento é um campo obrigatório.',
+            'telefone.required' => 'Telefone é um campo obrigatório.'
+        ];
+
+        $validateUser = Validator::make(
+            $request->all(),
+            [
+                'cpf' => 'required',
+                'email' => 'required|email',
+                'password' => 'required',
+                'data_nascimento' => 'required|date',
+                'telefone' => 'required'
+            ],
+            $messages
+        );
+
+        if ($validateUser->fails()) {
+            return response()->json(
+                [
+                    'status' => false,
+                    'error' => 'Erro de validação.',
+                    'message' => $validateUser->errors()
+                ]
+            );
+        }
+
+        try {
+            $user = new User;
+            $user->fill($request->all());
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            $token = $user->createToken('token_access')->plainTextToken;
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Usuário cadastrado com sucesso',
+                'token' => $token
+            ], 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+
+
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Request $request)
     {
-        //
+        $token = $request->header('Authorization');
+        $user = new ResourcesUser(Auth::guard('sanctum')->user());
+
+        return response()->json([
+            'status' => true,
+            'token' => $token,
+            'data' => $user
+        ], 200);
     }
 
     /**
@@ -49,9 +99,26 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request)
     {
-        //
+        try {
+            $user_id = Auth::guard('sanctum')->user()->id;
+            $user = User::find($user_id);
+            $user->fill($request->all());
+            $user->password = Hash::make($request->password);
+            $user->save();
+            $token = $request->header('Authorization');
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Usuário cadastrado com sucesso',
+                'token' => $token
+            ], 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+
+
     }
 
     /**
