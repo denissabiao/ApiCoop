2c88f049717ac469e3012ca0e144b5bd49fe6854
denissabiao <denis.sabiao@gmail.com>
2023-03-01 16:19:22 -0300

    Add Completed Model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'data_nascimento',
+        'cpf',
+        'telefone',
+        'telefone_adicional'
     ];
 
     /**
