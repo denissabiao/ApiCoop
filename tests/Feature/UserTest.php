beb88848084b24b1daa541d761c614e21f37e5cf
denissabiao <denis.sabiao@gmail.com>
2023-03-02 15:34:49 -0300

    Define user Test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -8,6 +8,30 @@ use Tests\TestCase;
 class UserTest extends TestCase
 {
 
+    public function test_create_user()
+    {
+        
+        $response = $this->postJson(
+            '/api/user',
+            [
+                "name" => "denis sabiao",
+                "email" => "denis3@gmail.com",
+                "password" => "password",
+                "data_nascimento" => "2022-12-12",
+                "cpf" => "1154211",
+                "telefone" => "35987452144",
+                "telefone_adicional" => "3698521444"
+            ]
+        );
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            "status" => true,
+            "message" => "Usuário cadastrado com sucesso"
+        ]);
+    }
+
     public function test_user_not_auth_cant_list_user()
     {
         $response = $this->getJson('/api/user/1');
@@ -34,6 +58,31 @@ class UserTest extends TestCase
             ]
         ]);
     }
+
+    public function test_user_auth_can_edit_user()
+    {
+        $response = $this->putJson(
+            '/api/user/1',
+            [
+                "name" => "denis sabiao",
+                "email" => "denis2@gmail.com",
+                "password" => "password",
+                "data_nascimento" => "2022-12-12",
+                "cpf" => "115471211",
+                "telefone" => "35987452144",
+                "telefone_adicional" => "3698521444"
+            ],
+            ['Authorization' => 'Bearer 2|myr6L2I7tx4Bnliv6ERZCaDYtFeQkQGq5vsn02JH']
+        );
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            "status" => true,
+            "message" => "Usuário editado com sucesso"
+        ]);
+    }
+ 
 
 
 }
